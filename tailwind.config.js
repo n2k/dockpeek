4e00d1d698215da7e3689f913e8e5805753c9d27
dockpeek <dockpeek@gmail.com>
2025-08-04 11:00:41 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './templates/*.html',
+    './static/js/app.js',
+    'app.py'
+  ],
+  safelist: [
+    'bg-red-500',
+    'text-xl',
+    'hover:underline'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

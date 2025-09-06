d98d9d996a00dbfdf44efa6a30330622105fd12f
dockpeek <dockpeek@gmail.com>
2025-09-06 05:23:37 +0200

    feat(ui): update Traefik column behavior

--- static/js/app.js
+++ static/js/app.js
@@ -257,11 +257,14 @@ document.addEventListener("DOMContentLoaded", () => {
         if (c.traefik_routes && c.traefik_routes.length > 0) {
           traefikCell.innerHTML = c.traefik_routes.map(route => {
             const displayUrl = route.url.replace(/^https?:\/\//, '');
-            return `<div class="traefik-route mb-1">
-            <a href="${route.url}" target="_blank" class="text-blue-600 hover:text-blue-800 text-sm" data-tooltip="${route.rule}">
-              ${displayUrl}
-            </a>
-          </div>`;
+            return `<div class="traefik-route mb-1" data-tooltip="${displayUrl}">
+  <a href="${route.url}" target="_blank" class="text-blue-600 hover:text-blue-800 text-sm">
+    <span class="traefik-text">${displayUrl}</span>
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M21 9L21 3M21 3H15M21 3L13 11M10 5H7.8C6.11984 5 5.27976 5 4.63803 5.32698C4.07354 5.6146 3.6146 6.07354 3.32698 6.63803C3 7.27976 3 8.11984 3 9.8V16.2C3 17.8802 3 18.7202 3.32698 19.362C3.6146 19.9265 4.07354 20.3854 4.63803 20.673C5.27976 21 6.11984 21 7.8 21H14.2C15.8802 21 16.7202 21 17.362 20.673C17.9265 20.3854 18.3854 19.9265 18.673 19.362C19 18.7202 19 17.8802 19 16.2V14" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  </a>
+</div>`;
           }).join('');
         } else {
           traefikCell.innerHTML = `<span class="status-none text-sm">none</span>`;
